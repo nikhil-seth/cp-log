cd2f5961b769eeff0acfb32425bb39d5918f4cb6
Nikhil Seth <rootedguy24@gmail.com>
2020-05-12 22:15:12 +0530

    Updates Sample

--- Sample.cpp
+++ Sample.cpp
@@ -1,13 +1,25 @@
 #include<bits/stdc++.h>
 #define ll long long
+#define UL unsigned long long
 #define FOR(i,a,b) for(auto i=(a);i<=(b);i++)
-#define elif else if
 #define FORA(x,arr) for(auto &x:arr)
-#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define elif else if
 #define pb push_back
 #define fio freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
-#define UL unsigned long
+#define vi vector<int>
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define mp(a,b) make_pair(a,b)
+#define mms(a) memset(a,0,sizeof(a))
 using namespace std;
 int main(){
+	fastio
+	#ifndef ONLINE_JUDGE
+	fio
+	#endif
+
+	
+	
 	return 0;
 }
